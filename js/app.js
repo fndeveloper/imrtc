0749c2c09a7bac40010274c749c34398d640d446
fndeveloper <fasihnasir60@gmail.com>
2025-02-19 20:24:58 +0500

    c

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
+// NAVBAR IS FIXED AFTER SCROLL IS GREATER THAN 800
 var navbar=document.getElementById("navbar")
-console.log(navbar);
-
 
 window.addEventListener("scroll",()=>{
   
@@ -10,6 +9,14 @@ window.addEventListener("scroll",()=>{
     else{
         navbar.classList.remove("fixed-nav")
     }
-  
-    
+  })
+// NAVBAR IS FIXED AFTER SCROLL IS GREATER THAN 800
+
+// FOOTER COPY RIGHT YEAR CODE IS HERE
+var year=document.querySelectorAll(".year")
+year.forEach((E)=>{
+    E.innerHTML=new Date().getFullYear()
 })
+
+// FOOTER COPY RIGHT YEAR CODE IS HERE
+

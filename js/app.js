650160349bdcd829bda52aaeb3b5ca8a399b6965
fndeveloper <fasihnasir60@gmail.com>
2025-02-19 21:40:35 +0500

    c

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 // NAVBAR IS FIXED AFTER SCROLL IS GREATER THAN 800
 var navbar=document.getElementById("navbar")
-
+var top_to_scrol_btn=document.querySelectorAll(".top_to_scrol_btn")
 window.addEventListener("scroll",()=>{
-  
+  top_to_scrol_btn.forEach((e)=>{
+    var a=this.document.body.scrollHeight -this.window.innerHeight;
+    var scrollProgress = (window.scrollY / a) * 99;
+
+
+if(scrollProgress > 97){
+    e.innerHTML=`  <i class="fa-solid fa-arrow-up"></i>`
+}
+else{
+    e.innerHTML=scrollProgress.toFixed(0)
+}
+})
     if(window.scrollY >100*8){
         navbar.classList.add("fixed-nav")
     }
